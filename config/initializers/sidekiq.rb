4309ef8ac3993134a27d3cf06528d6d350a457c2
galaxytourist <honghwajung@gmail.com>
2014-04-26 02:50:28 -0400

    sidekiq update

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -12,6 +12,32 @@ Sidekiq.configure_server do |config|
   end
 end
 
+if Rails.env.production?
+
+  require 'autoscaler/sidekiq'
+  require 'autoscaler/heroku_scaler'
+
+    Sidekiq.configure_server do |config|
+      config.redis = sidekiq_redis
+      config.server_middleware do |chain|
+        chain.add(Autoscaler::Sidekiq::Server, Autoscaler::HerokuScaler.new('sidekiq'), 60)
+      end
+    end
+
+    Sidekiq.configure_client do |config|
+      config.redis = sidekiq_redis
+      config.client_middleware do |chain|
+        chain.add Autoscaler::Sidekiq::Client, 'default' => Autoscaler::HerokuScaler.new('sidekiq')
+      end
+    end
+
+else
+
+  Sidekiq.configure_server { |config| config.redis = sidekiq_redis }
+  Sidekiq.configure_client { |config| config.redis = sidekiq_redis }
+
+end
+
 if Sidekiq.server?
 
   # warm up AR
